ca6def4600e13b9c7daab9b6e1a9d25adb0b08b8
Bhagya-Makhija <bhagyamakhija1@gmail.com>
2026-05-17 15:53:46 +0530

    First commit

---
+++ src/main/java/com/bhagya/meeting_notes_summarizer/MeetingNotesSummarizerApplication.java
@@ -0,0 +1,13 @@
+package com.bhagya.meeting_notes_summarizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingNotesSummarizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingNotesSummarizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bhagya/meeting_notes_summarizer/MeetingNotesSummarizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhagya.meeting_notes_summarizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingNotesSummarizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
